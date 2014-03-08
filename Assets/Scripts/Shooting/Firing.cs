e96dcd2e3ad2c46706ecd8ae76a647bace479a3d
Bryan Chong <mail@bryanchong.com>
2014-03-08 12:25:25 -0800

    Cannon shooting DONE

---
+++ Assets/Scripts/Shooting/Firing.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Firing : MonoBehaviour {
+	public int projectileSpeed = 50;
+	public Rigidbody projectile;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		Fire ();
+	}
+
+	void Fire (){
+		Rigidbody clone; 
+		if (Input.GetKeyDown(KeyCode.R)) {
+				clone = Instantiate (projectile, transform.position, transform.rotation) as Rigidbody;
+				clone.velocity = transform.TransformDirection (Vector3.forward * projectileSpeed);
+				Destroy (clone.gameObject, 5);
+		}
+	}
+}

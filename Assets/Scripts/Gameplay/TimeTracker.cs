da4a0bd0242c7176f7d91bd38f159add237e7811
Valakor <pohlmann@usc.edu>
2014-03-08 12:50:57 -0800

    Freezing objects mostly works

--- Assets/Scripts/Gameplay/TimeTracker.cs
+++ Assets/Scripts/Gameplay/TimeTracker.cs
@@ -1,36 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(Rigidbody))]
+
 public class TimeTracker : MonoBehaviour {
 	
 	//State = 1 means moving
 	//State = 0 means not moving
-	int m_state = 1;
+	/*int m_state = 1;
 	int m_lerpFactor = 0;
-	bool m_objStopped = false;
+	bool m_objStopped = false;*/
+	Vector3 oldVelocity = Vector3.zero;
+	Vector3 oldAngularVelocity = Vector3.zero;
+	bool oldUseGravity = false;
+	Rigidbody rBody = null;
 
 
-	public int State{
+	/*public int State{
 		set { m_state = value;}
 		get { return m_state;}
-	}
+	}*/
+	void Awake () {
 
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+		rBody = gameObject.GetComponent<Rigidbody>();
+		GameManager.Instance.addTimeObject(this);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (m_objStopped) {
-			Mathf.Lerp
-		}
 	
 	}
 
-	void StopObject(bool shouldStop){
-		m_objStopped = shouldStop;
+	public void StopObject() {
+		oldVelocity = rBody.velocity;
+		oldAngularVelocity = rBody.angularVelocity;
+		oldUseGravity = rBody.useGravity;
+		rBody.velocity = Vector3.zero;
+		rBody.angularVelocity = Vector3.zero;
+		rBody.useGravity = false;
+	}
+
+	public void ResumeObject() {
+		rBody.velocity = oldVelocity;
+		rBody.angularVelocity = oldAngularVelocity;
+		rBody.useGravity = oldUseGravity;
 	}
 }
--- Assets/Scripts/Utilities/GameManager.cs
+++ Assets/Scripts/Utilities/GameManager.cs
@@ -1,25 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : Singleton<GameManager> {
 	protected GameManager() {}
 
 	#region const defines
+	public float minVelocityBuffer = 0.0001f;
 	#endregion
 
 	#region playerStats
+	GameObject player = null;
+	CharacterController playerController = null;
+	#endregion
+
+	#region otherVariables
+	LinkedList<TimeTracker> timeObjects = null;
+	bool timeStopped = false;
 	#endregion
 
 	void Awake() {
-
+		timeObjects = new LinkedList<TimeTracker>();
 	}
 
 	void Start() {
-
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerController = player.GetComponent<CharacterController>();
 	}
 
 	void Update() {
+		float currentPlayerVelocity = playerController.velocity.sqrMagnitude;
 
+		if (timeStopped && currentPlayerVelocity > minVelocityBuffer) {
+			resumeTime();
+			timeStopped = false;
+		} else if (!timeStopped && currentPlayerVelocity < minVelocityBuffer) {
+			stopTime();
+			timeStopped = true;
+		}
+	}
+
+	public void addTimeObject(TimeTracker tt) {
+		timeObjects.AddLast(tt);
+	}
+
+	public void removeTimeObject(TimeTracker tt) {
+		timeObjects.Remove(tt);
+	}
+
+	void clearAllTimeObjects() {
+		timeObjects.Clear();
+	}
+
+	void stopTime() {
+		foreach (TimeTracker tt in timeObjects) {
+			tt.StopObject();
+		}
+	}
+
+	void resumeTime() {
+		foreach (TimeTracker tt in timeObjects) {
+			tt.ResumeObject();
+		}
 	}
 
 }

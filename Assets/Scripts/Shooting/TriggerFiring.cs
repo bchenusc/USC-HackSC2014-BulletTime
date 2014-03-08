bc979277764e2344718e78ebe9c42c23efadef39
Bryan Chong <mail@bryanchong.com>
2014-03-08 13:28:58 -0800

    Trap platforms and interval cannon shooting works

---
+++ Assets/Scripts/Shooting/TriggerFiring.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerFiring : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider platform){
+		if (platform.CompareTag("Player")){
+			Debug.Log("Enter!");
+			this.transform.parent.GetChild (0).GetChild (0).GetComponent<Firing> ().StartFiring ();
+		}
+	}
+
+	void OnTriggerExit(Collider platform){
+		if (platform.CompareTag("Player")){
+			Debug.Log("Exit!");
+			this.transform.parent.GetChild (0).GetChild (0).GetComponent<Firing> ().StopFiring ();
+		}
+	}
+
+}

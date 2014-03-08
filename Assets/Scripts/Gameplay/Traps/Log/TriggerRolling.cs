b24b457e0888530fe0263619f90478774b28d9b0
Bryan Chong <mail@bryanchong.com>
2014-03-08 14:25:20 -0800

    Logs are rolling, on my front lawn, they be acting like they're white and rolly

---
+++ Assets/Scripts/Gameplay/Traps/Log/TriggerRolling.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerRolling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider platform){
+		if (platform.CompareTag("Player")){
+			this.transform.parent.GetChild (0).GetComponent<Rolling>().Roll();
+		}
+	}
+	
+	void OnTriggerExit(Collider platform){
+		if (platform.CompareTag("Player")){
+
+		}
+	}
+
+}
